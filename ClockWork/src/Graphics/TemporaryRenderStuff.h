6809edfc9ed666a2b80da287ec054d08f6887480
ozgun <elpekozgun@gmail.com>
2023-04-01 14:06:51 +0300

    cleanup

--- ClockWork/src/Graphics/TemporaryRenderStuff.h
+++ ClockWork/src/Graphics/TemporaryRenderStuff.h
@@ -22,8 +22,9 @@ namespace CW
 	{
 	public:
 		std::vector<GLuint> indices;
+		std::vector<Texture> textures;
 
-		std::unique_ptr<Shader> shader;
+		std::unique_ptr<Shader> modelShader;
 		std::unique_ptr<Shader> lightShader;
 		std::unique_ptr<VAO> vao;
 		std::unique_ptr<VBO> vbo;
@@ -31,14 +32,9 @@ namespace CW
 		std::unique_ptr<VAO> lightVao;
 		std::unique_ptr<VBO> lightVbo;
 		std::unique_ptr<EBO> lightEbo;
-		//std::vector<unique_ptr<Texture>> Textures;
-		std::vector<Texture> textures;
 		std::unique_ptr<Mesh> mesh;
+		std::unique_ptr<Mesh> lightMesh;
 
-		/*std::unique_ptr<Texture> diffuseTexture;
-		std::unique_ptr<Texture> specularTexture;*/
-
-		// for now we keep a reference to camera component here.
 		CW::CameraComponent* camera;
 
 		TempRender()
@@ -93,29 +89,31 @@ namespace CW
 			indices.push_back(15);
 			indices.push_back(14);
 
-			//Textures.push_back(std::make_unique<Texture>(R"(C:\_Dev\ClockWork\ClockWork\res\Texture\crate.png)", "Diffuse", 0, GL_RGBA, GL_UNSIGNED_BYTE, GL_NEAREST, GL_REPEAT));
-			//Textures.push_back(std::make_unique<Texture>(R"(C:\_Dev\ClockWork\ClockWork\res\Texture\cratespecular.png)", "Specular", 1, GL_RGBA, GL_UNSIGNED_BYTE, GL_NEAREST, GL_REPEAT));
-
-
 			textures.push_back(Texture(R"(C:\_Dev\ClockWork\ClockWork\res\Texture\crate.png)", "Diffuse", 0, GL_RGBA, GL_UNSIGNED_BYTE, GL_NEAREST, GL_REPEAT));
 			textures.push_back(Texture(R"(C:\_Dev\ClockWork\ClockWork\res\Texture\cratespecular.png)", "Specular", 1, GL_RGBA, GL_UNSIGNED_BYTE, GL_NEAREST, GL_REPEAT));
 
 			mesh = std::make_unique<Mesh>(vertices, indices, textures);
 
-			
-			GLfloat lightVertices[] =
-			{ //     COORDINATES     //
-				-0.1f, -0.1f,  0.1f,
-				-0.1f, -0.1f, -0.1f,
-				 0.1f, -0.1f, -0.1f,
-				 0.1f, -0.1f,  0.1f,
-				-0.1f,  0.1f,  0.1f,
-				-0.1f,  0.1f, -0.1f,
-				 0.1f,  0.1f, -0.1f,
-				 0.1f,  0.1f,  0.1f
+			auto vertexShader = Shader::CreateShaderSource(R"(C:\_Dev\ClockWork\ClockWork\res\Shader\DefaultVertex.glsl)", ShaderType::Vertex);
+			auto fragmentShader = Shader::CreateShaderSource(R"(C:\_Dev\ClockWork\ClockWork\res\Shader\DefaultFragment.glsl)", ShaderType::Fragment);
+
+			modelShader = Shader::CreateShader("shader", { vertexShader, fragmentShader });
+
+			//-------------------------------------------
+
+			std::vector<Vertex> LightVertices
+			{
+				Vertex{glm::vec3(-0.1f, -0.1f,  0.1f)},
+				Vertex{glm::vec3(-0.1f, -0.1f, -0.1f)},
+				Vertex{glm::vec3(0.1f, -0.1f, -0.1f)},
+				Vertex{glm::vec3(0.1f, -0.1f,  0.1f)},
+				Vertex{glm::vec3(-0.1f,  0.1f,  0.1f)},
+				Vertex{glm::vec3(-0.1f,  0.1f, -0.1f)},
+				Vertex{glm::vec3(0.1f,  0.1f, -0.1f)},
+				Vertex{glm::vec3(0.1f,  0.1f,  0.1f)}
 			};
 
-			GLuint lightIndices[] =
+			std::vector<unsigned int> LightIndices
 			{
 				0, 1, 2,
 				0, 2, 3,
@@ -131,41 +129,11 @@ namespace CW
 				4, 6, 7
 			};
 
-			auto vertexShader = Shader::CreateShaderSource(R"(C:\_Dev\ClockWork\ClockWork\res\Shader\DefaultVertex.glsl)", ShaderType::Vertex);
-			auto fragmentShader = Shader::CreateShaderSource(R"(C:\_Dev\ClockWork\ClockWork\res\Shader\DefaultFragment.glsl)", ShaderType::Fragment);
-
-			shader = Shader::CreateShader("shader", { vertexShader, fragmentShader });
-
-			vao = std::make_unique<VAO>();
-			vao->Bind();
-
-			vbo = std::make_unique<VBO>(vertices);
-			ebo = std::make_unique<EBO>(indices);
-
-			vao->LinkAttribArray<float>(*vbo, 0, 3, GL_FLOAT, 11, 0);
-			vao->LinkAttribArray<float>(*vbo, 1, 3, GL_FLOAT, 11, 3);
-			vao->LinkAttribArray<float>(*vbo, 2, 3, GL_FLOAT, 11, 6);
-			vao->LinkAttribArray<float>(*vbo, 3, 2, GL_FLOAT, 11, 9);
-
-			vao->Unbind();
-			vbo->Unbind();
-			ebo->Unbind();
-
 			auto lightVertex = Shader::CreateShaderSource(R"(C:\_Dev\ClockWork\ClockWork\res\Shader\LightVertex.glsl)", ShaderType::Vertex);
 			auto lightFragment = Shader::CreateShaderSource(R"(C:\_Dev\ClockWork\ClockWork\res\Shader\LightFragment.glsl)", ShaderType::Fragment);
 			lightShader = Shader::CreateShader("shader", { lightVertex, lightFragment });
 
-			lightVao = std::make_unique<VAO>();
-			lightVao->Bind();
-
-			lightVbo = std::make_unique<VBO>(lightVertices, sizeof(lightVertices));
-			lightEbo = std::make_unique<EBO>(lightIndices, sizeof(lightIndices));
-
-			lightVao->LinkAttribArray<float>(*lightVbo, 0, 3, GL_FLOAT, 3, 0);
-
-			vao->Unbind();
-			vbo->Unbind();
-			ebo->Unbind();
+			lightMesh = std::make_unique<Mesh>(LightVertices, LightIndices, std::vector<Texture>());
 
 			glm::vec4 lightColor = glm::vec4(1.0f, 1.0f, 1.0f, 1.0f);
 			glm::vec3 lightPos = glm::vec3(0.5f, 0.5f, 0.5f);
@@ -174,30 +142,23 @@ namespace CW
 
 			glm::mat4 model = glm::mat4(1.0f);
 
-			shader->Use();
-			shader->setVec4("LightColor", lightColor);
-			shader->setMat4("Model", model);
+			modelShader->Use();
+			modelShader->setVec4("LightColor", lightColor);
+			modelShader->setMat4("Model", model);
 			
-			shader->SetFloat("Shineness", 32.0f);
-			shader->setVec3("pointLights[0].position", lightPos);
-			shader->setVec3("pointLights[0].diffuse", 1.0f, 1.0f, 1.0f);
-			shader->setVec3("pointLights[0].ambient", 0.15f, 0.15f, 0.15f);
-			shader->setVec3("pointLights[0].specular", 1.0f, 1.0f, 1.0f);
-			shader->SetFloat("pointLights[0].Kconstant", 1.0f);
-			shader->SetFloat("pointLights[0].Klinear", 0.09f);
-			shader->SetFloat("pointLights[0].Kquad", 0.032f);
-			shader->SetBool("IsBlinnPhong", true);
-
-
-			// Order of samplers is important here.
-			shader->SetTexture("Diffuse0", 0); 
-			shader->SetTexture("Specular0", 1);
-
+			modelShader->SetFloat("Shineness", 32.0f);
+			modelShader->setVec3("pointLights[0].position", lightPos);
+			modelShader->setVec3("pointLights[0].diffuse", 1.0f, 1.0f, 1.0f);
+			modelShader->setVec3("pointLights[0].ambient", 0.15f, 0.15f, 0.15f);
+			modelShader->setVec3("pointLights[0].specular", 1.0f, 1.0f, 1.0f);
+			modelShader->SetFloat("pointLights[0].Kconstant", 1.0f);
+			modelShader->SetFloat("pointLights[0].Klinear", 0.09f);
+			modelShader->SetFloat("pointLights[0].Kquad", 0.032f);
+			modelShader->SetBool("IsBlinnPhong", true);
 
 			lightShader->Use();
 			lightShader->setVec4("LightColor", lightColor);
 			lightShader->setMat4("Model", lightModel);
-
 		}
 
 		void Render(float dt)
@@ -207,28 +168,23 @@ namespace CW
 
 			auto camMat = camera->CameraMatrix();
 
-			shader->Use();
-			shader->setMat4("CamMat", camMat);
-			shader->setVec3("eyePosition", camera->Position);
+			modelShader->Use();
+			modelShader->setMat4("CamMat", camMat);
+			modelShader->setVec3("eyePosition", camera->Position);
+			mesh->Draw(*modelShader, camMat, camera->Position);
 
-			mesh->Draw(*shader, camMat, camera->Position);
-
-			//vao->Bind();
-			//glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
 
 			glm::vec3 lightPos = glm::vec3(0.5f, 0.5f, 0.5f);
-
 			lightShader->Use();
 			lightShader->setMat4 ("CamMat", camMat);
+			lightMesh->Draw(*lightShader, camMat, camera->Position);
 
-			lightVao->Bind();
-			glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
 		}
 
 		~TempRender()
 		{
 			mesh.reset();
-			//delete[] indices;
+			lightMesh.reset();
 		}
 	};
 }
